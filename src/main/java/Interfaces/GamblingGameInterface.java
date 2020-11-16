a0475cda2991b135d201ef8b68191d2d1a472cf3
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-16 18:11:29 -0500

    added Interfaces classes and package

---
+++ src/main/java/Interfaces/GamblingGameInterface.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+public interface GamblingGameInterface {
+
+    void payout();
+
+    void takeBets();
+}
---
+++ src/main/java/Interfaces/Game.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface Game {
+
+    void handleTurns();
+
+    void addPlayer();
+
+    void removePlayer();
+}
