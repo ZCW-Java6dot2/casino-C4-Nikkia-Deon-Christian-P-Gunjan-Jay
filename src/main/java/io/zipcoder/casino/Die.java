56d86264cab4b2702eeadd9abae475f9f0ddaf81
deon <deon@zipcodes-MacBook-Pro-23.local>
2020-11-17 10:05:21 -0500

    Die and DiceGame class

---
+++ src/main/java/io/zipcoder/casino/Die.java
@@ -0,0 +1,24 @@
+package io.zipcoder.casino;
+
+import java.util.Random;
+
+
+public class Die {
+    private Integer numberOfSides;
+    private Random random;
+
+    public Die(Integer numberOfSides){
+        this.numberOfSides = numberOfSides;
+        this.random = random;
+    }
+
+    public Integer getNumberOfSides(Integer numberOfSides) {
+        return numberOfSides;
+    }
+
+    public Integer rollDie() {
+        Integer dieValue = random.nextInt(numberOfSides) + 1;
+
+        return dieValue;
+    }
+}
