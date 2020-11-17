02c5df7c6e5aab6e7ff87660b32b744ebbc36b03
nikki <nikki@Gbrads-Mac.local>
2020-11-17 11:29:41 -0500

    Player constructors

--- src/main/java/io/zipcoder/casino/Player.java
+++ src/main/java/io/zipcoder/casino/Player.java
@@ -10,8 +10,26 @@ public class Player {
     private ArrayList<Card> hand;
     private String name;
     private Card card;
+
     public Player(){
         hand = new ArrayList<Card>();
+    }
+
+    // name only constructor
+
+    public Player(String name){
+        this.name = name;
+    }
+    // name, balance, hand constructor
+    public Player(String name, Double balance, ArrayList<Card> hand){
+        this.name = name;
+        this.balance = balance;
+        this.hand = hand;
+    }
+    //name, balance constructor
+    public Player(String name, Double balance){
+        this.name = name;
+        this.balance = balance;
     }
 
     public Double playerWager(Double wager){
@@ -29,37 +47,34 @@ public class Player {
     public void setBalance(Double balance) {
         this.balance = balance;
     }
+
     public double getBalance(){
         return balance;
     }
+
     public ArrayList<Card> getHand() {
         return hand;
     }
+
     public void addCard(Card c){
         hand.add(c);
     }
+
     public void setHand(ArrayList<Card> hand) {
 
         this.hand = hand;
     }
-
+    
 
     public void removeCard(Card c){
         hand.remove(c);
     }
 
     public Card getCard(){
-    return card;
+        return card;
     }
 
     public void setCard(Card card){
-    this.card = card;
+        this.card = card;
     }
-
-
-
-
-
-
-
 }
