d8ad28fb7babcc8cae82f73e5e0c6890d3a2c0af
Jay <73120876+JarrellW@users.noreply.github.com>
2020-11-18 11:03:45 -0500

    updates to highroller

--- src/main/java/io/zipcoder/casino/HighRoller.java
+++ src/main/java/io/zipcoder/casino/HighRoller.java
@@ -3,17 +3,18 @@ package io.zipcoder.casino;
 import io.zipcoder.casino.diceclasses.Die;
 import io.zipcoder.casino.utilities.Console;
 
-import java.util.Scanner;
+import java.util.Random;
+
 
 public class HighRoller {
-    Scanner scanner = new Scanner;
     Die dice;
-    Player player;
+    HighRollerPlayer player;
     HighRollerNpc npc1;
     HighRollerNpc npc2;
     HighRollerNpc npc3;
+    Double prizePool = 0.0;
     private static Console console = new Console(System.in, System.out);
-    public HighRoller(Player player, HighRollerNpc npc1, HighRollerNpc npc2, HighRollerNpc npc3) {
+    public HighRoller(HighRollerPlayer player, HighRollerNpc npc1, HighRollerNpc npc2, HighRollerNpc npc3) {
         dice = new Die(6);
         this.player = player;
         this.npc1 = npc1;
@@ -29,16 +30,43 @@ public class HighRoller {
                 System.out.println("Nah fam not today");
                 gameStart();
             }
+            player.getPlayer().setBalance(player.getPlayer().getBalance() - userInput);
+            prizePool += userInput;
+            System.out.println("Npcs place your bet");
+            prizePool += npcBet(npc1);
+            prizePool += npcBet(npc2);
+            prizePool += npcBet(npc3);
+            System.out.println("Roll your dice");
+            player.setCurrentRoll(diceRoll());
+            System.out.println(player.getPlayer().getName() + "Rolled " + player.getCurrentRoll());
+            npc1.setCurrentRoll(diceRoll());
+            System.out.println("Rolled " + npc1.getCurrentRoll());
+            npc2.setCurrentRoll(diceRoll());
+            System.out.println("Rolled " + npc2.getCurrentRoll());
+            npc3.setCurrentRoll(diceRoll());
+            System.out.println("Rolled " + npc3.getCurrentRoll());
         } catch (Exception e){
             System.out.println("Dealer throws out player");
             gameStart();
         }
 
-
     }
+    public Double npcBet(HighRollerNpc npc){
+        double x = (Math.random()*((10-50)+1))+10;
+        npc.setWallet(npc.getWallet() - x);
+        return x;
+    }
+
     public Integer diceRoll(){
+        Integer value = dice.rollDie() + dice.rollDie();
+        return value;
+    }
+    public String getWinner(){
+        String winner = "";
 
     }
+
+
 
 
 }
--- src/main/java/io/zipcoder/casino/HighRollerPlayer.java
+++ src/main/java/io/zipcoder/casino/HighRollerPlayer.java
@@ -1,23 +1,17 @@
 package io.zipcoder.casino;
 
-public class HighRollerNpc {
-   private Player player;
-    private Double wallet;
+public class HighRollerPlayer {
     private Integer currentRoll;
+    private Player player;
     private Boolean activeRoller;
 
-
-    public HighRollerNpc(Player player,Double startingMoney) {
+    public HighRollerPlayer(Player player){
         this.player = player;
-        this.wallet = startingMoney;
+
     }
 
-    public Double getWallet() {
-        return wallet;
-    }
-
-    public void setWallet(Double wallet) {
-        this.wallet = wallet;
+    public Player getPlayer() {
+        return player;
     }
 
     public Integer getCurrentRoll() {
